ac1d182890a0e732e6ccf78d1809c796d797ba39
Rachel Lew <rlew07@gmail.com>
2020-02-23 16:11:00 -0700

    Add more attribute columns to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_171657) do
+ActiveRecord::Schema.define(version: 2020_02_23_223045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2020_02_22_171657) do
     t.datetime "updated_at", null: false
     t.string "refresh_token"
     t.string "email"
+    t.string "provider"
+    t.string "uid"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "oauth_expires_at"
   end
 
 end
