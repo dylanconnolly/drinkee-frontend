cdeb259325d8a8efb9a36e4f486f0526169f0649
David Gomez <davdg25@gmail.com>
2020-02-24 14:25:55 -0700

    creat ingredient model to turn all ingredients from json to ruby objects

--- app/poros/ingredient.rb
+++ app/poros/ingredient.rb
@@ -1,6 +1,10 @@
 class Ingredient
+  attr_reader :id, :name, :description, :alcohol, :abv
   def initialize(ingredient_data)
-    # @name = ingredient_data
-    # @id = ingredient_data
+    @id = ingredient_data[:attributes][:id]
+    @name = ingredient_data[:attributes][:name]
+    @description = ingredient_data[:attributes][:description]
+    @alcohol = ingredient_data[:attributes][:alcohol]
+    @abv = ingredient_data[:attributes][:abv]
   end
 end
