82cbecdebefb206a32634a4792f6fca5efd5b51b
David Gomez <davdg25@gmail.com>
2020-02-24 14:31:48 -0700

    remove hashed out example

--- app/services/back_end_api.rb
+++ app/services/back_end_api.rb
@@ -12,14 +12,3 @@ class BackEndApi
     ingredients
   end
 end
-
-
-# class OpenLibraryApi
-#
-#   def connection
-#     response = Faraday.get('http://openlibrary.org/search.json?title=the+man+who+saw+everything')
-#
-#     book_info = JSON.parse(response.body, symbolize_names: true)[:docs].first
-#     Book.new(book_info)
-#   end
-# end
