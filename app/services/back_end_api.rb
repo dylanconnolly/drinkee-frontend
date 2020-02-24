267cb1d0a5be18ff434caad10be519cb66a7278c
David Gomez <davdg25@gmail.com>
2020-02-24 14:26:25 -0700

    create service which pull from out backend

--- app/services/back_end_api.rb
+++ app/services/back_end_api.rb
@@ -3,8 +3,13 @@ class BackEndApi
     response = Faraday.get('http://localhost:9393/ingredients')
 
     ingredient_info = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+    ingredients  = []
+    (ingredient_info)[:data].each do |ingredient_data|
 
+      ingredients.push Ingredient.new(ingredient_data)
+
+    end
+    ingredients
   end
 end
 
