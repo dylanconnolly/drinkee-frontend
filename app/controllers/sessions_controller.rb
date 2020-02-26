71b943f49c82c38516659d5033731dc8a08adbfe
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-26 11:59:00 -0700

    Add helper method to create a user cabinet on the backend when a new user is created
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,18 @@ class SessionsController < ApplicationController
     auth_hash = request.env["omniauth.auth"]
     user = User.update_or_create(auth_hash)
     session[:id] = user.id
+    create_user_cabinet_on_backend(user)
     redirect_to '/dashboard'
   end
 
   def destroy
     session.clear
     redirect_to '/'
+  end
+
+  private
+
+  def create_user_cabinet_on_backend(user)
+    BackEndApi.new.create_cabinet(user.id)
   end
 end
