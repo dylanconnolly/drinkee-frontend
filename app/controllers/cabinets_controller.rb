7258a8d39992244d8a77e2784412f231add79651
David Gomez <davdg25@gmail.com>
2020-02-25 19:17:31 -0700

    create update controller for ingredients form
    Co-Authored-By: Rachel Lew <rlew421@users.noreply.github.com>

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -1,5 +1,10 @@
 class CabinetsController < ApplicationController
   def show
-    
+
+  end
+
+  def update
+    session[:ingredients_array] = params[:ingredients]
+    redirect_to('/cabinet')
   end
 end
