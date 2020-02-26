afab983c7247a1a4a1c29d92a5137194c35af8fa
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-26 10:19:29 -0700

    Add patch request to api backend in cabinet update action
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -5,6 +5,7 @@ class CabinetsController < ApplicationController
 
   def update
     session[:ingredients_array] = params[:ingredients]
+    BackEndApi.new.send_ingredients(session[:ingredients_array], current_user.id)
     redirect_to('/cabinet')
   end
 end
