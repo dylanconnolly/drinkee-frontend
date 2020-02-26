9a223a524a77440840a36197da4f779784508e12
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-26 11:59:22 -0700

    Add methods for getting cabinet and creating cabinet for the backend
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- app/services/back_end_api.rb
+++ app/services/back_end_api.rb
@@ -21,10 +21,25 @@ class BackEndApi
     ingredients
   end
 
+  def get_cabinet(user_id)
+    cabinet_ingredients_info = get_json("api/v1/#{user_id}/cabinet")
+    ingredients = []
+    cabinet_ingredients_info[:data].each do |ingredient_data|
+
+      ingredients.push Ingredient.new(ingredient_data)
+
+    end
+    ingredients
+  end
+
   def send_ingredients(ingredient_ids_array, user_id)
 
     json = ingredient_ids_array.to_json
 
     connection.patch(url = "api/v1/#{user_id}/cabinet", body = json)
+  end
+
+  def create_cabinet(user_id)
+    connection.post("/api/v1/#{user_id}/cabinet")
   end
 end
