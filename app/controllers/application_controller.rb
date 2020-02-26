e3c170f7abffa2a7d2a3dd123f553d4fcb632320
David Gomez <davdg25@gmail.com>
2020-02-25 19:17:11 -0700

    create application method to always be able to call current_ingredients
    Co-Authored-By: Rachel Lew <rlew421@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :current_ingredients_id
 
   def current_user
     @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+  def current_ingredients_id
+    session[:ingredients_array] ||= []
   end
 end
