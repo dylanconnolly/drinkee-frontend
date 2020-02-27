800b25b74f7e43d00c4ddaf5a636a405860f6ba7
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-27 11:55:51 -0700

    Add index action to results controller
    Co-Authored-By: null <DavidBarriga-Gomez@users.noreply.github.com>

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,7 @@
 class ResultsController < ApplicationController
   def index
     render file: '/public/404' unless current_user
+
+    @results = BackEndApi.new.get_results(current_user.id)
   end
 end
