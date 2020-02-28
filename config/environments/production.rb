e355d7385beae40abc1cb223c47826968ef8c053
Rachel Lew <rlew07@gmail.com>
2020-02-27 17:44:24 -0700

    Change port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
-    port:                 '465',
+    port:                 '25',
     domain:               'drinkee-frontend.herokuapp.com',
     user_name:            ENV["SENDGRID_USERNAME"],
     password:             ENV["SENDGRID_PASSWORD"],
